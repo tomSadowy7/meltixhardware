beb2e9ac9c64bf022cd86626792d0a408d0a66aa
tsadowy1 <tsadowy@princeton.edu>
2025-03-12 18:43:35 -0400

    added coming soon

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState} from 'react'
 
 import Header from './HeaderFolder/Header.js'
+import ComingSoon from './ComingSoon/ComingSoon.js'
 function App() {
   return(<>
-    <Header />
+    <ComingSoon />
     </>)
 }
 
---
+++ my-app/client/src/ComingSoon/ComingSoon.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import './ComingSoon.css'
+import MeltixLogo from '../assets/MeltixLogo.png'
+
+function ComingSoon(){
+
+    return(<html>
+    <body>
+        
+        <img className="fade-in"src={MeltixLogo}></img>
+        <h1 className="fade-text"> COMING SOON </h1>
+        
+    </body>
+    
+    </html>);
+
+}
+export default ComingSoon
