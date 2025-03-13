c56aa3982165930ffbd734709ded090da635f085
tsadowy1 <tsadowy@princeton.edu>
2025-03-12 21:09:02 -0400

    Version 1.2.2 added title

--- my-app/client/src/ComingSoon/ComingSoon.js
+++ my-app/client/src/ComingSoon/ComingSoon.js
@@ -6,6 +6,7 @@ function ComingSoon(){
 
     return(<html>
         <head>
+            <title>Meltix Technologies</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
         <body>
