9fc2136570cd94afc2e0499d2041c16b12a319ac
tsadowy1 <tsadowy@princeton.edu>
2025-03-12 20:55:34 -0400

    version 1.1 added hardware background

--- my-app/client/src/ComingSoon/ComingSoon.js
+++ my-app/client/src/ComingSoon/ComingSoon.js
@@ -5,14 +5,20 @@ import MeltixLogo from '../assets/MeltixLogo.png'
 function ComingSoon(){
 
     return(<html>
-    <body>
-        
-        <img className="fade-in"src={MeltixLogo}></img>
-        <h1 className="fade-text"> COMING SOON </h1>
-        
-    </body>
-    
-    </html>);
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </head>
+        <body>
+            <div class="background"/>
+            <div class="content">
+                <img className="fade-in"src={MeltixLogo}></img>
+                    <h1 className="fade-text"> Join the New Generation</h1>
+                    <h2 className="fade-text">Coming Soon </h2>
+                    <h3 className="fade-text">Email info@meltixtech.com for inquiries</h3>
+            </div>
+        </body>
+        </html>
+        );
 
 }
 export default ComingSoon
