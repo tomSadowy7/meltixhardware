35b38ee2c1173a49824c6690a97fd9aad86153eb
tsadowy1 <tsadowy@princeton.edu>
2025-05-18 19:52:12 -0400

    added protection

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "Geo";
 const char* password = "Guallpa1";
@@ -7,12 +8,38 @@ const char* password = "Guallpa1";
 const int ledPin = 33; // Use GPIO 33
 WebServer server(80);
 
+const char* correctKey = "123456";  // Replace with your actual key
+
 void handleFlash() {
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  server.send(200, "text/plain", "GPIO 33 flashed");
+  if (server.hasArg("plain") == false) {
+    server.send(400, "text/plain", "Missing body");
+    return;
+  }
+
+  String body = server.arg("plain");
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, body);
+
+  if (error) {
+    server.send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+  
+  const char* key = doc["key"];
+
+  Serial.println("received");
+  Serial.println(key);
+
+  if (key && String(key) == correctKey) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    server.send(200, "text/plain", "Flashed");
+  } else {
+    server.send(403, "text/plain", "Unauthorized");
+  }
 }
+
 
 void setup() {
   Serial.begin(115200);
@@ -30,7 +57,7 @@ void setup() {
   Serial.println("\nConnected to WiFi");
   Serial.println(WiFi.localIP());
 
-  server.on("/flash", handleFlash);
+  server.on("/flash", HTTP_POST, handleFlash);
   server.begin();
 }
 
